79b7cff41bba0e4141d863fd1d00b316a1573270
Panagiotis Roubatsis <proubatsis@gmail.com>
2017-04-15 21:48:18 -0400

    Setup api structure

--- Program.fs
+++ Program.fs
@@ -10,24 +10,28 @@ open System.Net
 let serialize o =
     JsonConvert.SerializeObject(o)
 
-let getAllTodoLists s: WebPart =
-    fun (x: HttpContext) ->
-        async {
-            let! lists = fetchAllTodoLists()
-            return! OK (s lists) x
-        }
+let getAllTodoLists ctx =
+    async {
+        let! lists = fetchAllTodoLists()
+        return! OK (serialize lists) ctx
+    }
 
 let jsonMime = setMimeType "application/json; charset=utf-8"
 
 [<EntryPoint>]
 let main argv =
+    let api =
+        [
+            GET >=> path "/todos" >=> getAllTodoLists >=> jsonMime
+        ] |> choose
+
     let rec start (port: uint16) =
         let config = 
             { defaultConfig with 
                 bindings=[ HttpBinding.create HTTP (IPAddress.Parse("0.0.0.0")) port ]
             } in
             try
-                startWebServer config ((getAllTodoLists serialize) >=> jsonMime)
+                startWebServer config api
                 0
             with
                 | :? Sockets.SocketException -> start (port + 1us)
